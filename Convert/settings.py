5381ee25430ca8509580b773ef894afdfa3a26ac
Saatvik2001 <arvapallysaatvik@gmail.com>
2021-06-20 18:24:20 +0530

    save

--- Convert/settings.py
+++ Convert/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7)m3_mr69@=$2uue^&0$83f3!#qoso_$((bn0ryk*7z%&ri^c)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vcetoolkit.herokuapp.com']
 
 
 # Application definition
